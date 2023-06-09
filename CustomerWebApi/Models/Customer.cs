7654f1a23d0493918ddbd30699bad020d43ef899
bombehjort <mart243g@easv365.dk>
2023-06-09 04:05:30 +0200

    Added customer microservice and docker orchestration

---
+++ CustomerWebApi/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CustomerWebApi.Models
+{
+    [Table("customer", Schema = "dbo")]
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("name")]
+        public string Name { get; set; }
+        [Column("email")]
+        public string Email { get; set; }
+        [Column("phone")]
+        public string Phone { get; set; }
+    }
+}
--- CustomerWebApi/Program.cs
+++ CustomerWebApi/Program.cs
@@ -1,3 +1,6 @@
+using CustomerWebApi;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+/* Database Context Dependency Injection */
+var dbHost = "localhost";
+var dbName = "dms_customer";
+var dbPassword = "admin";
+var connectionstring = $"Data Source = {dbHost}, Initial Catalog = {dbName}; User ID=sa; Password = {dbPassword}";
+builder.Services.AddDbContext<CustomerDbContext>(opt => opt.UseSqlServer(connectionstring));                                       
+
 
 var app = builder.Build();
 
